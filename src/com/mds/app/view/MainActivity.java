7873111a6c9b576e796caa08c6383102a5d9d6af
Caio Nardelli Maranhao <caio.nardelli@hotmail.com>
2013-09-26 18:19:49 -0300

    indentando e refatorando pacote view

--- src/com/mds/app/view/MainActivity.java
+++ src/com/mds/app/view/MainActivity.java
@@ -1,27 +1,22 @@
 package com.mds.app.view;
 
-import com.mds.app.R;
-import com.mds.app.R.layout;
-import com.mds.app.R.menu;
-import com.mds.app.controller.ProjetoController;
-
-import android.os.Bundle;
 import android.app.Activity;
-import android.app.Dialog;
+import android.os.Bundle;
 import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.LinearLayout;
-import android.widget.RelativeLayout;
-import android.widget.TextView;
+
+import com.mds.app.R;
+import com.mds.app.controller.ProjetoController;
 
 public class MainActivity extends Activity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		
+
 		final EditText textSigla = new EditText(this);
 		final EditText textAno = new EditText(this);
 		textSigla.setHint("Insira um tipo de projeto");
@@ -30,21 +25,20 @@ public class MainActivity extends Activity {
 		button.setWidth(100);
 		button.setHeight(50);
 		button.setText("Procurar");
-        LinearLayout layout = new LinearLayout(this);
-        layout.addView(textSigla);
-        layout.addView(textAno);
-        layout.addView(button);
-       
-        setContentView(layout);
-        
-        final ProjetoController projC = new ProjetoController();
-        
-        button.setOnClickListener(new View.OnClickListener(){
-        	public void onClick(View arg0) {
-        		projC.atualizarDadosDaPesquisa(textAno.getText().toString(),
-        				textSigla.getText().toString());
-        	}
-        });
+		LinearLayout layout = new LinearLayout(this);
+		layout.addView(textSigla);
+		layout.addView(textAno);
+		layout.addView(button);
+
+		setContentView(layout);
+
+		final ProjetoController projetoController = new ProjetoController();
+
+		button.setOnClickListener(new View.OnClickListener() {
+			public void onClick(View arg0) {
+				projetoController.atualizarDadosDaPesquisa(textAno.getText().toString(), textSigla.getText().toString());
+			}
+		});
 
 	}
 
