fde0e4b6d70c6ac72ba90ece9c6bb3ee9cb03b9f
cleitoncsg <cleiton.tom@hotmail.com>
2013-10-03 23:19:55 -0300

    Subindo os exemplos de ArrayList
    Aula de ArrayList realizada no dia 3 de outubro para melhor o desempenho
    do time APP.

---
+++ src/com/mds/app/examples/Time.java
@@ -0,0 +1,23 @@
+package com.mds.app.examples;
+
+public class Time {
+	private String nome;
+
+	public Time(String nome) {
+		this.nome = nome;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	@Override
+	public String toString() {
+		return " [nome=" + nome + "]";
+	}
+	
+}
