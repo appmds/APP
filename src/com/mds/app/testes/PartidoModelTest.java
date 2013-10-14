8c4a1311c4ab15f3a2faa15ff6792470c0e06479
JohnnysRibeiro <johnnys.ribeiro@gmail.com>
2013-10-14 16:56:07 -0300

    testes PartidoModel

---
+++ src/com/mds/app/testes/PartidoModelTest.java
@@ -0,0 +1,79 @@
+package com.mds.app.testes;
+
+import java.util.ArrayList;
+
+import junit.framework.TestCase;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import com.mds.app.model.ParlamentarModel;
+import com.mds.app.model.PartidoModel;
+
+public class PartidoModelTest extends TestCase {
+
+	private ParlamentarModel parlamentarModel;
+	private PartidoModel partidoModel;
+	private ArrayList<ParlamentarModel> parlamentares;
+	private ArrayList<ParlamentarModel> outrosParlamentares;
+
+	@Before
+	public void setUp() throws Exception {
+		System.out.println("Comecando o teste...");
+
+		// Criando o partido de teste
+		parlamentares = new ArrayList<ParlamentarModel>();
+		partidoModel = new PartidoModel(parlamentares, "PMDS", "DF");
+		
+		outrosParlamentares = new ArrayList<ParlamentarModel>();
+	}
+
+	@After
+	public void tearDown() throws Exception {
+		System.out.println("Terminando...");
+	}
+
+	@Test
+	public void testGetParlamentares() {
+		assertSame(parlamentares, partidoModel.getParlamentares());
+	}
+	
+	@Test
+	public void testGetSiglaPartido(){
+		assertEquals("PMDS", partidoModel.getSiglaPartido());
+	}
+	
+	@Test
+	public void testGetUf(){
+		assertEquals("DF", partidoModel.getUf());
+	}
+	
+	@Test
+	public void testSetParlamentares() {
+		partidoModel.setParlamentares(outrosParlamentares);
+		assertSame(outrosParlamentares, partidoModel.getParlamentares());
+	}
+	
+	@Test
+	public void testSetSiglaPartido(){
+		partidoModel.setSiglaPartido("PGPP");
+		assertEquals("PGPP", partidoModel.getSiglaPartido());
+	}
+	
+	@Test
+	public void testSetUf(){
+		partidoModel.setUf("MG");
+		assertEquals("MG", partidoModel.getUf());
+	}
+	
+	@Test
+	public void testAddParlamentar(){
+		parlamentarModel = new ParlamentarModel();
+		outrosParlamentares.add(parlamentarModel);
+		partidoModel.addParlamentar(parlamentarModel);
+		assertEquals(outrosParlamentares, partidoModel.getParlamentares());
+		
+	}
+
+}
