4838019ba9288992bfa7f426fe398beee08f597c
JohnnysRibeiro <johnnys.ribeiro@gmail.com>
2013-10-14 15:55:14 -0300

    Fazendo os testes funcionarem

---
+++ src/com/mds/app/testes/ParlamentarModelTest.java
@@ -0,0 +1,38 @@
+package com.mds.app.testes;
+
+import java.util.ArrayList;
+
+import junit.framework.TestCase;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import com.mds.app.model.ParlamentarModel;
+import com.mds.app.model.PartidoModel;
+
+public class ParlamentarModelTest extends TestCase{
+	
+	private ParlamentarModel parlamentarModel;
+	private PartidoModel partidoModel;
+
+	@Before
+	public void setUp() throws Exception {
+		System.out.println("Comecando o teste...");
+		
+		ArrayList<ParlamentarModel> parlamentares = new ArrayList<ParlamentarModel>();
+		partidoModel = new PartidoModel(parlamentares, "PMDS", "DF");
+	}
+
+	@After
+	public void tearDown() throws Exception {
+		System.out.println("Terminando...");
+	}
+
+	@Test
+	public void testGetNome() {
+		parlamentarModel = new ParlamentarModel("NomeParlamentar", partidoModel);
+		assertEquals("NomeParlamentar", parlamentarModel.getNome());
+	}
+
+}
