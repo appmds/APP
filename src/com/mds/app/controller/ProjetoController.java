6e6cf12894e5621d48b906fe76acb88610007c58
simiaosimis <simiaosimis@hotmail.com>
2013-10-14 13:44:18 -0300

    Concertando mostrar projetos

--- src/com/mds/app/controller/ProjetoController.java
+++ src/com/mds/app/controller/ProjetoController.java
@@ -6,8 +6,7 @@ import org.xml.sax.Attributes;
 import org.xml.sax.SAXException;
 import org.xml.sax.helpers.DefaultHandler;
 
-import com.mds.app.model.ParlamentarModel;
-import com.mds.app.model.PartidoModel;
+import com.mds.app.model.ProcuraProjetoModel;
 import com.mds.app.model.ProjetoModel;
 import com.mds.app.persistencia.Persistencia;
 
@@ -16,12 +15,7 @@ public class ProjetoController extends DefaultHandler {
 	private StringBuffer buffer = new StringBuffer();
 
 	private ArrayList<ProjetoModel> listaProjetos;
-	private ArrayList<PartidoModel> listaPartidos;
-	private ArrayList<ParlamentarModel> listaParlamentares;
 	private ProjetoModel projeto;
-	private PartidoModel partido;
-	private ParlamentarModel parlamentar;
-	
 
 	public ProjetoController() {
 
@@ -29,26 +23,16 @@ public class ProjetoController extends DefaultHandler {
 
 	ArrayList<ProjetoModel> projetosInstanciados = Persistencia.instanciarProjetos();
 
-	private int validacaoPartido;
-	private int validacaoParlamentar;
-
 	@Override
 	public void startElement(String namespaceURI, String localName, String qName, Attributes atts) throws SAXException {
 
 		buffer.setLength(0);
-		validacaoPartido = 1;
-		validacaoParlamentar = 1;
 
 		if (localName.equals("proposicoes")) {
 			listaProjetos = new ArrayList<ProjetoModel>();
-			listaPartidos = new ArrayList<PartidoModel>();
-			listaParlamentares = new ArrayList<ParlamentarModel>();
 		}
 		else if (localName.equals("proposicao")) {
 			projeto = new ProjetoModel();
-		}
-		else if (localName.equals("txtSiglaPartido")){
-			partido = new PartidoModel();
 		}
 		else {
 			//
@@ -57,15 +41,13 @@ public class ProjetoController extends DefaultHandler {
 
 	@Override
 	public void endElement(String uri, String localName, String qName) throws SAXException {
-		
+
 		if (localName.equals("proposicao")) {
 			listaProjetos.add(projeto);
 		}
-		
 		else if (localName.equals("nome")) {
 			projeto.setNome(buffer.toString());
 		}
-		
 		else if (localName.equals("sigla")) {
 			projeto.setSigla(buffer.toString());
 		}
@@ -78,43 +60,27 @@ public class ProjetoController extends DefaultHandler {
 		else if (localName.equals("txtEmenta")) {
 			projeto.setExplicacao(buffer.toString());
 		}
-/*	TENTATIVA DE MELHORAR A LÓGICA VERIFICAR SE A IDEIA ESTÁ CERTA E CORRIGIR	
-		else if (localName.equals("txtSiglaPartido")){
-			for(int i=0;i<listaPartidos.size();i++){
-				if(listaPartidos.get(i).getSiglaPartido() == buffer.toString())
-					validacaoPartido = 0;
-					partido = listaPartidos.get(i);
-			}
-			if (validacaoPartido == 1)
-				partido.setSiglaPartido(buffer.toString());
-		}
-		else if (localName.equals("txtSiglaUF")){
-			if (validacaoPartido == 1)
-				partido.setUf(buffer.toString());
-		}
-		else if (localName.equals("txtNomeAutor")) {
-			for(int i=0;i<listaParlamentares.size();i++){
-				if(listaParlamentares.get(i).getNome() == buffer.toString())
-					validacaoParlamentar = 0;
-				    projeto.setParlamentar(listaParlamentares.get(i));
-			}
-			if (validacaoParlamentar==1){
-				parlamentar = new ParlamentarModel();
-				parlamentar.setNome(buffer.toString());
-				parlamentar.setPartido(partido);
-				listaParlamentares.add(parlamentar);
-				projeto.setParlamentar(parlamentar);
-			}
-		}
-		*/
-		
 		else {
 
 		}
 	}
 
+	@Override
+	public void characters(char[] ch, int start, int length) {
+		buffer.append(ch, start, length);
+	}
+
 	public ArrayList<ProjetoModel> getListaProjetos() {
 		return listaProjetos;
+	}
+
+	public void atualizarDadosDaPesquisa(String ano, String sigla, String numero, String dataIni,
+			String dataFinal, String autor, String nomeAutor, String siglaPartido, String uf) {
+
+		ProcuraProjetoModel.setAno(ano);
+		ProcuraProjetoModel.setSigla(sigla);
+		ProcuraProjetoModel.setDataInicio(dataIni);
+
 	}
 
 }
