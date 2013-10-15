45bb81d6aa6d31ef1a0a0c34799edaea2357a97d
Caio Nardelli Maranhao <caio.nardelli@hotmail.com>
2013-10-14 21:25:00 -0300

    Fazendo leitura de arquivo da persistencia

--- src/com/mds/app/persistencia/Persistencia.java
+++ src/com/mds/app/persistencia/Persistencia.java
@@ -1,15 +1,56 @@
 package com.mds.app.persistencia;
 
-import java.util.ArrayList;
+import java.io.IOException;
+import java.io.InputStream;
 
-import com.mds.app.model.ParlamentarModel;
-import com.mds.app.model.PartidoModel;
-import com.mds.app.model.ProjetoModel;
+import android.app.Activity;
+import android.content.Context;
+import android.content.res.AssetManager;
 
-public abstract class Persistencia {
+public class Persistencia extends Activity {
+
+	/*
+	 * Pra ler a persistencia:
+	 * 
+	 * Persistencia persistencia = new Persistencia();
+	 * persistencia.lerPersistencia(NomeDaClasse.this);
+	 * String conteudoTxt = persistencia.getTxtContent();
+	 */
+
+	private String txtContent;
 
 	public Persistencia() {
-
+		txtContent = "";
 	}
-		
+
+	public void lerPersistencia(Context context) {
+		final String txtPersistencia = "PL2013.txt";
+		byte[] buffer = null;
+		InputStream is;
+		AssetManager manager;
+
+		try {
+			manager = context.getAssets();
+			is = manager.open(txtPersistencia);
+			int size = is.available(); // tamanho do arquivo em bytes
+			buffer = new byte[size];
+			is.read(buffer);
+			is.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		String str_data = new String(buffer);
+
+		this.setTxtContent(str_data);
+	}
+
+	public String getTxtContent() {
+		return txtContent;
+	}
+
+	public void setTxtContent(String txtContent) {
+		this.txtContent = txtContent;
+	}
+
 }
