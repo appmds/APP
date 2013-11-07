5568f64ed0c8044a18585ceed656e6cd3d5dd259
simiaosimis <simiaosimis@hotmail.com>
2013-11-07 15:47:37 -0200

    Melhorando solução da lista

--- src/com/mds/app/view/Lista.java
+++ src/com/mds/app/view/Lista.java
@@ -1,7 +1,6 @@
 package com.mds.app.view;
 
 import java.util.ArrayList;
-import java.util.List;
 
 import android.app.ActionBar.LayoutParams;
 import android.app.Activity;
@@ -11,7 +10,9 @@ import android.graphics.Color;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.LinearLayout;
+import android.widget.ListView;
 import android.widget.ScrollView;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -19,69 +20,50 @@ import android.widget.Toast;
 import com.mds.app.R;
 import com.mds.app.controller.ListaController;
 import com.mds.app.controller.ProposicaoController;
-import com.mds.app.model.ProjetoModel;
+import com.mds.app.util.StableArrayAdapter;
 
 public class Lista extends Activity {
-	ArrayList<ListElement> elementos = new ArrayList<ListElement>();
-	private ScrollView MainView;
-	private LinearLayout ViewLayout;
-	private LinearLayout ViewText;
-	private ProposicaoController pesquisa;
+	
 	private ListaController listar;
 	private ArrayList<String> stringProjetos;
-	private ArrayList<TextView> textViews = new ArrayList<TextView>();
 
-	public Lista() {
-	}
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-
-		MainView = new ScrollView(this);
-		ViewLayout = new LinearLayout(this);
-		ViewLayout.setOrientation(LinearLayout.VERTICAL);
-		ViewText = new LinearLayout(this);
-		ViewText.setOrientation(LinearLayout.VERTICAL);
+		
+		ListView listview = new ListView(this);
+		
 
 		Intent intent = getIntent();
 		intent.getExtras().getStringArrayList("PROJETOS");
 		stringProjetos = intent.getStringArrayListExtra("PROJETOS");
+		
+		final StableArrayAdapter adapter = new StableArrayAdapter(this,
+		        android.R.layout.simple_list_item_1, stringProjetos);
+		    listview.setAdapter(adapter);
 
-		for (int i = 0; i < 9; i++) {
-			if (i % 2 == 0) {
-				elementos.add(new ListElement(this, "YELLOW"));
-			}
-			else {
-				elementos.add(new ListElement(this, "BLUE"));
-			}
-			elementos.get(i).setLayoutParams(new LinearLayout.LayoutParams(LayoutParams.MATCH_PARENT, 100));
-		}
+		    listview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
 
-		for (int i = 0; i < stringProjetos.size(); i++) {
-			TextView text = new TextView(this);
-			text.setY(i * 50);
-			text.setText(stringProjetos.get(i));
-			textViews.add(text);
-		}
+		      @Override
+		      public void onItemClick(AdapterView<?> parent, final View view,
+		          int position, long id) {
+		        final String item = (String) parent.getItemAtPosition(position);
+		        view.animate().setDuration(2000).alpha(0)
+		            .withEndAction(new Runnable() {
+		              @Override
+		              public void run() {
+		            	  stringProjetos.remove(item);
+		                adapter.notifyDataSetChanged();
+		                view.setAlpha(1);
+		              }
+		            });
+		      }
+		    });
 
-		for(int i = 0; i<textViews.size(); i++){
-			ViewText.addView(textViews.get(i));
-		}
+		setContentView(listview);
 
-		for (int i = 0; i < 9; i++) {
-			ViewLayout.addView(elementos.get(i));
-		}
-
-		ViewLayout.setLayoutParams(new LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT));
-		ViewLayout.requestLayout();
-		// ViewText.setLayoutParams(new LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT));
-		ViewText.requestLayout();
-
-		MainView.addView(ViewText);
-		setContentView(MainView);
-
-	}
+	} 	
 
 	class ListElement extends View {
 		public ListElement(Context context, String TypeName) {
@@ -110,9 +92,5 @@ public class Lista extends Activity {
 
 	public ListaController getListaController() {
 		return listar;
-	}
-
-	public void setListaController(ListaController listar) {
-		this.listar = listar;
 	}
 }
