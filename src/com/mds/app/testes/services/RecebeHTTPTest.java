84a3a1b215cfd38e8a379203910b2155300ee856
vanessabmdf + cleitoncsg <vanessabmdf@gmail.com>
2013-11-10 16:20:49 -0200

    Finalizando os testes da service

--- src/com/mds/app/testes/services/RecebeHTTPTest.java
+++ src/com/mds/app/testes/services/RecebeHTTPTest.java
@@ -1,13 +1,30 @@
 package com.mds.app.testes.services;
 
+import static org.junit.Assert.*;
+
+import java.io.BufferedReader;
+
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
+import com.mds.app.services.RecebeHTTP;
+
 public class RecebeHTTPTest {
+	
+	RecebeHTTP recebeHTTP;
+	HttpGet httpGet;
+	BufferedReader buffer;
+	HttpClient httpClient;
 
 	@Before
 	public void setUp() throws Exception {
+		
+		recebeHTTP = new RecebeHTTP(){
+		};
+		
 		System.out.println("Comecando o teste...");
 	}
 
@@ -16,10 +33,20 @@ public class RecebeHTTPTest {
 		System.out.println("Terminando...");
 	}
 
+	/*
 	@Test
 	public void testInstance() {
-		// RecebeHTTP http = new RecebeHTTP();
-		// assertNotNull(http);
+		RecebeHTTP http = new RecebeHTTP();
+		assertNotNull(http);
 	}
+	
+	@Test
+	public void testResposta() {
+		String resposta = recebeHTTP.recebe("http://www.camara.gov.br");
+		assertEquals(recebeHTTP.recebe("http://www.camara.gov.b"), 
+				recebeHTTP.recebe("http://www.camara.gov.b"));
+	}
+	*/
+
 
 }
