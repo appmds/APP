829d446bf5bf3abfd27511777559c4b2779bdf4a
Caio Nardelli Maranhao <caio.nardelli@hotmail.com>
2013-11-10 21:39:10 -0200

    Subindo exemplo de teste na view (Sobre)
    Detalhes: https://developer.android.com/tools/testing/activity_testing.html

---
+++ src/com/mds/app/testes/view/SobreTest.java
@@ -0,0 +1,33 @@
+package com.mds.app.testes.view;
+
+import org.junit.Test;
+
+import android.test.ActivityInstrumentationTestCase2;
+import android.widget.ImageButton;
+
+import com.mds.app.R;
+import com.mds.app.view.Sobre;
+
+public class SobreTest extends ActivityInstrumentationTestCase2<Sobre> {
+
+	private Sobre mActivity;
+	private ImageButton voltarTest;
+
+	public SobreTest() {
+		super(Sobre.class);
+	}
+
+	@Override
+	protected void setUp() throws Exception {
+		super.setUp();
+		mActivity = this.getActivity();
+		voltarTest = (ImageButton) mActivity.findViewById(R.id.voltar);
+	}
+
+	@Test
+	public void test() {
+		assertEquals(mActivity.voltar ,voltarTest);
+	}
+	
+
+}
