94b5bc077b66b62bb097740a06d43f3d60a3c245
vanessabmdf + cleitoncsg <cleiton.tom@hotmail.com>
2013-11-10 18:06:00 -0200

    Subindo mais testes da util

---
+++ src/com/mds/app/testes/util/CancelTaskOnCancelListenerTest.java
@@ -0,0 +1,30 @@
+package com.mds.app.testes.util;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import android.app.SearchManager.OnCancelListener;
+
+import com.mds.app.util.CancelTaskOnCancelListener;
+
+public class CancelTaskOnCancelListenerTest {
+
+	//Tem que descobrir como faz o teste...
+	
+	//CancelTaskOnCancelListener cancelTask;
+	//OnCancelListener onCancelListener;
+	
+	@Before
+	public void setUp() throws Exception {
+		//cancelTask = new CancelTaskOnCancelListener(null);
+	}
+
+	@After
+	public void tearDown() throws Exception {
+		System.out.println("Terminando");
+	}
+
+}
