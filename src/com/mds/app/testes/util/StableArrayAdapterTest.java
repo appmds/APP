117199eed0bc873b84ab8eec466c24506e128d92
vanessabmdf + cleitoncsg <vanessabmdf@gmail.com>
2013-11-10 16:29:28 -0200

    Iniciando o teste da util

---
+++ src/com/mds/app/testes/util/StableArrayAdapterTest.java
@@ -0,0 +1,27 @@
+package com.mds.app.testes.util;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import com.mds.app.util.StableArrayAdapter;
+
+public class StableArrayAdapterTest {
+
+	//StableArrayAdapter saa;
+	
+
+	@Before
+	public void setUp() throws Exception {
+		//saa = new StableArrayAdapter(null, 0, null);
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+}
--- src/com/mds/app/util/StableArrayAdapter.java
+++ src/com/mds/app/util/StableArrayAdapter.java
@@ -9,7 +9,7 @@ import android.widget.ArrayAdapter;
 public class StableArrayAdapter extends ArrayAdapter<String> {
 
 	HashMap<String, Integer> mIdMap = new HashMap<String, Integer>();
-
+	
 	public StableArrayAdapter(Context context, int textViewResourceId, ArrayList<String> objects) {
 		super(context, textViewResourceId, objects);
 		for (int i = 0; i < objects.size(); ++i) {
