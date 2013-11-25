0772d547704f73593626cc6207b3efbf8367f719
CaioIcy <caio.nardelli@hotmail.com>
2013-11-25 17:56:45 -0200

    Implementando HorizontalScrollView do sobre

--- view/com/mds/app/view/Sobre.java
+++ view/com/mds/app/view/Sobre.java
@@ -1,11 +1,8 @@
 package com.mds.app.view;
 
 import android.app.Activity;
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
-import android.view.View;
-import android.view.View.OnClickListener;
 import android.widget.ImageButton;
 
 import com.mds.app.R;
@@ -18,8 +15,6 @@ public class Sobre extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_sobre);
-
-		voltar_addListener();
 	}
 
 	@Override
@@ -27,23 +22,6 @@ public class Sobre extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.sobre, menu);
 		return true;
-	}
-
-	private void voltar_addListener() {
-		voltar = (ImageButton) findViewById(R.id.voltar);
-		voltar.setOnClickListener(new OnClickListener() {
-
-			@Override
-			public void onClick(View v) {
-				Intent i = new Intent(Sobre.this, MenuPrincipal.class);
-				startActivity(i);
-
-			}
-		});
-	}
-
-	public ImageButton getVoltar() {
-		return voltar;
 	}
 
 }
